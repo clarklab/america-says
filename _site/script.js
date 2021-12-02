3eb5365fe34f07d1934f82328dd641693439f2d0
Clark Wimberly <clark@clarklab.net>
2021-12-02 12:49:35 -0600

    merge

--- _site/script.js
+++ _site/script.js
@@ -1,40 +1,21 @@
+// setup sound effect for correct answers
+var audioCorrect = new Audio('../correct.m4a');
+
+// setup sound effect for wrong answers
+var audioWrong = new Audio('../wrong.m4a');
+
+// reveal the correct answer and play sound effect on click 
 $('.answer').on('click', function(e) {
   $(this).toggleClass("reveal");
+  audioCorrect.pause();
+  audioCorrect.currentTime = 0;
+  audioCorrect.play();
 });
 
-var audioElement = document.createElement('audio');
-audioElement.setAttribute('src', '../correct.m4a');
-
-var audioElementWrong = document.createElement('audio');
-audioElementWrong.setAttribute('src', '../wrong.m4a');
-
-
-
-//audioElement.load()
-$.get();
-audioElement.addEventListener("load", function() {
-audioElement.play();
-}, true);
-
-$.get();
-audioElementWrong.addEventListener("load", function() {
-audioElementWrong.play();
-}, true);
-
-
-
-$('.answer').click(function() {
-audioElement.play();
-});
-
-
-$('.pause').click(function() {
-audioElement.pause();
-});
-
-
+// keyboard shortcuts
 $(document).on('keypress',function(e) {
 
+// navigation between questions
   if ( e.ctrlKey && ( e.which === 49 ) ) {
     window.location.href = "question-1.html";
   }
@@ -47,38 +28,52 @@ $(document).on('keypress',function(e) {
     window.location.href = "question-3.html";
   }
 
-    if(e.which == 32) {
-      audioElementWrong.play();
-      $(".wrong").animate({opacity:1},100).delay(100).animate({opacity:0},100)
-    }
+  if ( e.ctrlKey && ( e.which === 52 ) ) {
+    window.location.href = "question-4.html";
+  }
 
-    if(!e.ctrlKey && e.which == 49) {
-      $(".answer:first-child").click();
-    }
+  if ( e.ctrlKey && ( e.which === 53 ) ) {
+    window.location.href = "question-5.html";
+  }
 
-    if(!e.ctrlKey && e.which == 50) {
-      $(".answer:nth-child(2)").click();
-    }
+  if ( e.ctrlKey && ( e.which === 54 ) ) {
+    window.location.href = "question-6.html";
+  }
 
-    if(!e.ctrlKey && e.which == 51) {
-      $(".answer:nth-child(3)").click();
-    }
+// play sound effect and flash X for wrong answer
+  if(e.which == 32) {
+    audioWrong.play();
+    $(".wrong").animate({opacity:1},100).delay(100).animate({opacity:0},100)
+  }
 
-    if(!e.ctrlKey && e.which == 52) {
-      $(".answer:nth-child(4)").click();
-    }
+// reveal correct answers and play sound effect
+  if(!e.ctrlKey && e.which == 49) {
+    $(".answer:first-child").click();
+  }
 
-    if(!e.ctrlKey && e.which == 53) {
-      $(".answer:nth-child(5)").click();
-    }
+  if(!e.ctrlKey && e.which == 50) {
+    $(".answer:nth-child(2)").click();
+  }
 
-    if(!e.ctrlKey && e.which == 54) {
-      $(".answer:nth-child(6)").click();
-    }
+  if(!e.ctrlKey && e.which == 51) {
+    $(".answer:nth-child(3)").click();
+  }
 
-    if(!e.ctrlKey && e.which == 55) {
-      $(".answer:nth-child(7)").click();
-    }
+  if(!e.ctrlKey && e.which == 52) {
+    $(".answer:nth-child(4)").click();
+  }
+
+  if(!e.ctrlKey && e.which == 53) {
+    $(".answer:nth-child(5)").click();
+  }
+
+  if(!e.ctrlKey && e.which == 54) {
+    $(".answer:nth-child(6)").click();
+  }
+
+  if(!e.ctrlKey && e.which == 55) {
+    $(".answer:nth-child(7)").click();
+  }
 });
 
 
